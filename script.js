59b53f52e965c82a238e504f21d545faaeb5b489
Manavi1427 <manavisarkar136@gmail.com>
2024-07-12 21:31:06 +0530

    js improvement

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 let cart=[]
 function cart(id,name,price){
+    const existing_item=cart.find(item=>item.id===id);
+    //if the item already exists, increase the quatity
+    if(existing_item){                          
+        existing_item+=1;
+    }
+    //if the item doesnt exist, add the item
+    else{
+        const newItem = { id, name, price, quantity: 1 };
+        cart.push(newItem);
+    }
+}
+update_cart_display();
+function update_cart_display(){
     
 }
