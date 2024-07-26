c0161372aa22240f6f971a3830e0899f3083238f
Manavi1427 <manavisarkar136@gmail.com>
2024-07-26 20:20:39 +0530

    page design

--- new-arrivals.js
+++ new-arrivals.js
@@ -1,8 +1,11 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("header1").style.marginLeft="250px";
+    document.getElementById("fashion").style.marginLeft="250px";
 }
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("header1").style.marginLeft="0";
+    document.getElementById("fashion").style.marginLeft="0";
 }
+
